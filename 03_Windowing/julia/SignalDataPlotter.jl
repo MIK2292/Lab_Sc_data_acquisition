6add020cc5fa46a083ee07c80f8e869f0dab115b
Luca-Cosca <csclcr02p28h919i@studenti.unical.it>
2024-11-06 19:10:19 +0100

    aggiunta script per calcolare largezza picchi

--- 03_Windowing/julia/SignalDataPlotter.jl
+++ 03_Windowing/julia/SignalDataPlotter.jl
@@ -13,7 +13,7 @@ function flattop(N)
 end
 
 #scala temporale
-time_scale = "_2ms.csv"
+time_scale = "_10ms.csv"
 
 #Lettura del segnale
 segnale = CSV.read(raw"03_Windowing\raw_data\Segnale\sig"*time_scale, DataFrame, header=13)
@@ -40,11 +40,11 @@ sin_hanning = sin .* hanning_window
 
 #Calcolo della FFT
 #fft_blackman = abs.(fft(sin_blackman))
-fft_blackman = fft(sin_blackman)
-fft_flattop = fft(sin_flattop)
-fft_hamming = fft(sin_hamming)
-fft_rectangle = fft(sin_rectangle)
-fft_hanning = fft(sin_hanning)
+fft_blackman =    log.(abs.(fft(sin_blackman)))
+fft_flattop =     log.(abs.(fft(sin_flattop)))
+fft_hamming =     log.(abs.(fft(sin_hamming)))
+fft_rectangle =   log.(abs.(fft(sin_rectangle)))
+fft_hanning =     log.(abs.(fft(sin_hanning)))
 
 #asse delle frequenze
 Fs = 1/(time[2]-time[1]) 
@@ -54,11 +54,11 @@ freq = Fs*(0:N-1)/N
 lim = (0, 10e3)
 
 #plot delle FFT
-p1 = plot(freq, abs.(fft_blackman), xlim = lim, title = "Blackman", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p2 = plot(freq, abs.(fft_flattop), xlim = lim, title = "Flattop", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p3 = plot(freq, abs.(fft_hamming), xlim = lim, title = "Hamming", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p4 = plot(freq, abs.(fft_rectangle), xlim = lim, title = "Rectangle", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p5 = plot(freq, abs.(fft_hanning), xlim = lim, title = "Hanning", xlabel = "Frequence [Hz]", ylabel = "Amplitude [dBV]")
+p1 = plot(freq, fft_blackman, xlim = lim, title = "Blackman", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
+p2 = plot(freq, fft_flattop, xlim = lim, title = "Flattop", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
+p3 = plot(freq, fft_hamming, xlim = lim, title = "Hamming", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
+p4 = plot(freq, fft_rectangle, xlim = lim, title = "Rectangle", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
+p5 = plot(freq, fft_hanning, xlim = lim, title = "Hanning", xlabel = "Frequence [Hz]", ylabel = "Amplitude [dBV]")
 
 combined = plot(p1, p2, p3, p4, p5, layout = (5,1), size = (800, 1000), legend = false, linewidth = 0.89)
 
