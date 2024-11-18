3086f95ab17191704439e6601b2c69ebc2962c92
Luca-Cosca <csclcr02p28h919i@studenti.unical.it>
2024-11-18 22:51:05 +0100

    script 03

--- 03_Windowing/julia/SignalDataPlotter.jl
+++ 03_Windowing/julia/SignalDataPlotter.jl
@@ -1,8 +1,18 @@
 using Plots
-plotlyjs()
-using CSV
+using CSV , LaTeXStrings
 using DataFrames
 using DSP, FFTW
+gr()
+
+# #set latex strings
+# plot_font = "Computer Modern"
+# default(fontfamily=plot_font,
+#         linewidth=2, framestyle=:box, label=nothing, grid=false)
+# scalefontsizes(1)
+
+plot_font = "Computer Modern"
+default(fontfamily=plot_font,
+        linewidth=4, framestyle=:box, label=nothing, grid=false)
 
 #creazione della finestra flattop
 function flattop(N)
@@ -12,8 +22,33 @@ function flattop(N)
     return w
 end
 
+#funzione per analizzare la larghezza dei picchi
+function peaks_width_finder(data_set, freq)
+    
+    subset = data_set[1:10000]
+
+    max = maximum(subset)
+    index = argmax(subset)
+
+    treshold = max/2 #valore di soglia(metà del picco)
+    
+    left_index = index
+    while left_index > 1 && data_set[left_index] > treshold
+        left_index -= 1
+    end
+
+    right_index = index
+    while right_index < length(data_set) && data_set[right_index] > treshold
+        right_index += 1
+    end
+
+    peakwidth = freq[right_index] - freq[left_index]
+
+    return max, index , peakwidth , left_index , right_index
+end
+
 #scala temporale
-time_scale = "_10ms.csv"
+time_scale = "_50ms.csv"
 
 #Lettura del segnale
 segnale = CSV.read(raw"03_Windowing\raw_data\Segnale\sig"*time_scale, DataFrame, header=13)
@@ -40,26 +75,54 @@ sin_hanning = sin .* hanning_window
 
 #Calcolo della FFT
 #fft_blackman = abs.(fft(sin_blackman))
-fft_blackman =    log.(abs.(fft(sin_blackman)))
-fft_flattop =     log.(abs.(fft(sin_flattop)))
-fft_hamming =     log.(abs.(fft(sin_hamming)))
-fft_rectangle =   log.(abs.(fft(sin_rectangle)))
-fft_hanning =     log.(abs.(fft(sin_hanning)))
+fft_list = [20*log10.(abs.(fft(sin_blackman))),
+            20*log10.(abs.(fft(sin_flattop))),
+            20*log10.(abs.(fft(sin_hamming))),
+            20*log10.(abs.(fft(sin_rectangle))),
+            20*log10.(abs.(fft(sin_hanning)))
+]
 
 #asse delle frequenze
 Fs = 1/(time[2]-time[1]) 
 freq = Fs*(0:N-1)/N
 
-#limite asse x
-lim = (0, 10e3)
+#set di liste utili
+window_types = ["Blackman","Flattop","Hamming","Rectangle","Hanning"]
 
-#plot delle FFT
-p1 = plot(freq, fft_blackman, xlim = lim, title = "Blackman", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p2 = plot(freq, fft_flattop, xlim = lim, title = "Flattop", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p3 = plot(freq, fft_hamming, xlim = lim, title = "Hamming", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p4 = plot(freq, fft_rectangle, xlim = lim, title = "Rectangle", xlabel = "Frequence[Hz]", ylabel = "Amplitude [dBV]")
-p5 = plot(freq, fft_hanning, xlim = lim, title = "Hanning", xlabel = "Frequence [Hz]", ylabel = "Amplitude [dBV]")
 
-combined = plot(p1, p2, p3, p4, p5, layout = (5,1), size = (800, 1000), legend = false, linewidth = 0.89)
+#parte del codice per ottenere il massimo e la larghezza del picco
+# max_blackman, index_blackman, width_blackman, left_blackman, right_blackman = peaks_width_finder(fft_blackman, freq)
+# max_flattop, index_flattop, width_flattop, left_flattop, right_flattop = peaks_width_finder(fft_flattop, freq)
+# max_hamming, index_hamming, width_hamming, left_hamming, right_hamming = peaks_width_finder(fft_hamming, freq)
+# max_rectangle, index_rectangle, width_rectangle, left_rectangle, right_rectangle = peaks_width_finder(fft_rectangle, freq)
+# max_hanning, index_hanning, width_hanning, left_hanning, right_hanning = peaks_width_finder(fft_hanning, freq)
 
+#plotting
+plotz = []
+for (i , fft) in enumerate(fft_list)
+    p = plot(freq, fft, xlim = (750, 1250),
+             title = window_types[i], 
+             xlabel = "Frequence [Hz]", 
+             ylabel = "Amplitude[dbV]", 
+             bottom_margin = 5Plots.mm,
+             titlefont = (size = 16), 
+             left_margin = 5Plots.mm,
+             legend = false)
+    push!(plotz, p)
+
+ 
+ combined = plot(plotz..., 
+                size = (800, 1500),
+                layout = (5,1), 
+                linewidth = 0.89,
+                left_margin = 20Plots.mm,
+ )
+end
+# savefig(combined , "fft_signal_50ms.pdf")
 display(combined)
+
+
+          
+
+
+
